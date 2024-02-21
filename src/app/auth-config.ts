d516c3dbf512aaa16159dc3881626a6ff4cb1694
Smokey <81595313+smokeythemalamute@users.noreply.github.com>
2024-02-21 22:48:46 +0000

    chore: remove the todo list (#2)
    * chore: lint and build on commit
    
    * chore: remove the todo app

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -7,7 +7,7 @@
 
 import { LogLevel, Configuration, BrowserCacheLocation } from '@azure/msal-browser';
 
-const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 /**
  * Enter here the user flows and custom policies for your B2C application,
@@ -61,31 +61,23 @@ export const msalConfig: Configuration = {
                 console.log(message);
             },
             logLevel: LogLevel.Verbose,
-            piiLoggingEnabled: false
-        }
-    }
-}
+            piiLoggingEnabled: false,
+        },
+    },
+};
 
 /**
  * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
  */
- export const protectedResources = {
-  apiTodoList: {
-      endpoint: "https://localhost:44351/api/todolist",
-      scopes: {
-          read: ["https://postyfoxdev.b2clogin.com/TodoList/ToDoList.Read"],
-          write: ["https://postyfoxdev.b2clogin.com/TodoList/ToDoList.ReadWrite"]
-      }
-  }
-}
+export const protectedResources = {};
 
 /**
-* Scopes you add here will be prompted for user consent during sign-in.
-* By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
-* For more information about OIDC scopes, visit:
-* https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
-*/
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit:
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
 export const loginRequest = {
-  scopes: []
+    scopes: [],
 };
