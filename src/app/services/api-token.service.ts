5b1769c10a890810b793d93ac533c6c388568fc5
Andy Neillans <andy@neillans.co.uk>
2024-09-25 21:19:30 +0100

    Add env endpoint to services

--- src/app/services/api-token.service.ts
+++ src/app/services/api-token.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ApiTokenService {
-    private apiUrl = '${environment.endpoint}/Profile_GenerateAPIToken';
+    private apiUrl = `${environment.endpoint}/Profile_GenerateAPIToken`;
 
     constructor(private http: HttpClient) {}
 
--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ServicesService {
-    private servicesUrl = '${environment.endpoint}/Services_GetAvailable';
+    private servicesUrl = `${environment.endpoint}/Services_GetAvailable`;
 
     constructor(private http: HttpClient) {}
 
