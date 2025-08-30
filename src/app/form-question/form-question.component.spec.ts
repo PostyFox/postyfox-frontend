11391f275ff38d901c45b787e78b6899ae4e3fb9
Andy Neillans <andy@neillans.co.uk>
2025-08-30 16:45:23 +0100

    Fix npm tests

--- src/app/form-question/form-question.component.spec.ts
+++ src/app/form-question/form-question.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import { FormQuestionComponent } from './form-question.component';
+import { QuestionBase } from '../models/question-base';
 
 describe('FormQuestionComponent', () => {
     let component: FormQuestionComponent;
@@ -8,11 +10,20 @@ describe('FormQuestionComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [FormQuestionComponent],
+            imports: [FormQuestionComponent, ReactiveFormsModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(FormQuestionComponent);
         component = fixture.componentInstance;
+
+        // Set up required inputs with proper constructor parameters
+        const testQuestion = new QuestionBase('test', 'Test Question', 'textbox', false);
+        testQuestion.controlType = 'textbox'; // Ensure controlType is set
+        component.question = testQuestion;
+        component.form = new FormGroup({
+            test: new FormControl(''),
+        });
+
         fixture.detectChanges();
     });
 
